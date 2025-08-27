2e21975560d3b4b85426df0aace8b6e757f20e6e
Alina Istomina <alinaistomina@yandex.ru>
2025-08-27 23:19:50 +0300

    Исправлен тест когда поиск не выдает результатов

--- test_yandex.py
+++ test_yandex.py
@@ -6,6 +6,6 @@ def test_find_course (browser_settings, browser_open):
     print("Success")
 
 def test_find_wrong (browser_settings, browser_open):
-    browser.element('[id="text"]').should(be.blank).type('QAGURU').press_enter()
-    browser.element('[id="search-result"]').should(have.text('Курсы тестировщиков'))
+    browser.element('[id="text"]').should(be.blank).type('рмонмгнпдшгидшгирл').press_enter()
+    browser.element('[class="EmptySearchResults-Title"]').should(have.text('Ничего не нашли'))
     print("Success")
